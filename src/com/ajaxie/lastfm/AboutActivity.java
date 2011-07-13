f7940586a389527fa6079a46626351711de4f196
Christian Beier <dontmind@freeshell.org>
2011-07-13 19:46:56 +0200

    Add a flattr button to about screen.
    Needs to be filled out still...

--- src/com/ajaxie/lastfm/AboutActivity.java
+++ src/com/ajaxie/lastfm/AboutActivity.java
@@ -1,14 +1,8 @@
 package com.ajaxie.lastfm;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
@@ -20,7 +14,7 @@ public class AboutActivity extends Activity {
 			TextView versionText = (TextView)findViewById(R.id.version);
 
 			try {
-				versionText.setText("version " + getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName);
+				versionText.setText("Version " + getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName);
 			} catch (NameNotFoundException e) {
 				versionText.setText("Invalid version -- please check for update");
 			}
