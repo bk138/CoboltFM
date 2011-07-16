21fbee25f23510caf629d7600902630385b1b947
Christian Beier <dontmind@freeshell.org>
2011-07-17 01:22:50 +0200

    Remove some unneeded imports.

--- src/com/ajaxie/lastfm/PlayerThread.java
+++ src/com/ajaxie/lastfm/PlayerThread.java
@@ -8,7 +8,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -18,25 +17,18 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 import org.xmlpull.v1.XmlSerializer;
 
 import android.content.Context;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnBufferingUpdateListener;
 import android.os.ConditionVariable;
@@ -45,9 +37,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
-import com.ajaxie.lastfm.PlayerService.ErrorStatus;
 import com.ajaxie.lastfm.PlayerService.LastFMNotificationListener;
-import com.ajaxie.lastfm.Utils.OptionsParser;
 import com.ajaxie.lastfm.Utils.ParseException;
 
 public class PlayerThread extends Thread {
