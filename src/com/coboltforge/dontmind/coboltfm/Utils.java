62115df2849d9b7326066de488b353f4d9d84e66
Christian Beier <dontmind@freeshell.org>
2011-10-07 04:43:55 +0200

    Fix some warnings.

--- src/com/coboltforge/dontmind/coboltfm/Utils.java
+++ src/com/coboltforge/dontmind/coboltfm/Utils.java
@@ -1,11 +1,6 @@
 package com.coboltforge.dontmind.coboltfm;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
