2f9fd431e897d2aacb2398154fd5777062b6e86d
Christian Beier <dontmind@freeshell.org>
2012-01-12 15:54:55 +0100

    Fix crash.

--- src/com/coboltforge/dontmind/coboltfm/PlayerThread.java
+++ src/com/coboltforge/dontmind/coboltfm/PlayerThread.java
@@ -436,17 +436,22 @@ public class PlayerThread extends Thread {
 	}
 
 	private XSPFTrackInfo getNextTrack() {
-		if (mPlaylist.size() == 0)
+		try {
+			if (mPlaylist.size() == 0)
+				return null;
+			if (mNextPlaylistItem >= mPlaylist.size()) {
+				mPlaylist = getPlaylist();
+				mNextPlaylistItem = 1;
+				return mPlaylist.get(0);
+			} else {
+				mNextPlaylistItem++;
+				if (mNextPlaylistItem == mPlaylist.size() - 1)
+					updatePlaylistDelayed();
+				return mPlaylist.get(mNextPlaylistItem - 1);
+			}
+		}
+		catch(NullPointerException e) {
 			return null;
-		if (mNextPlaylistItem >= mPlaylist.size()) {
-			mPlaylist = getPlaylist();
-			mNextPlaylistItem = 1;
-			return mPlaylist.get(0);
-		} else {
-			mNextPlaylistItem++;
-			if (mNextPlaylistItem == mPlaylist.size() - 1)
-				updatePlaylistDelayed();
-			return mPlaylist.get(mNextPlaylistItem - 1);
 		}
 	}
 
