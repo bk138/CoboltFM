d54c737cad0486785aaf285e76b3ae0683fcac63
Christian Beier <dontmind@freeshell.org>
2012-04-04 01:10:32 +0200

    Catch every exception in option parser.

--- src/com/coboltforge/dontmind/coboltfm/Utils.java
+++ src/com/coboltforge/dontmind/coboltfm/Utils.java
@@ -41,7 +41,7 @@ public class Utils {
 					mMap.put(name, value);
 				}
 				return true;
-			} catch (IOException e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				return false;
 			}
